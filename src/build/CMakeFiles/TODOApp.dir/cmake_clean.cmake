1d245bf0e6318daf831eb34e4458856f5018b68b
Andy <andres.meneguzzo@gmail.com>
2024-12-24 01:30:46 +0100

    CMake added

---
+++ src/build/CMakeFiles/TODOApp.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TODOApp.dir/Task.cpp.o"
+  "CMakeFiles/TODOApp.dir/Task.cpp.o.d"
+  "CMakeFiles/TODOApp.dir/TaskManager.cpp.o"
+  "CMakeFiles/TODOApp.dir/TaskManager.cpp.o.d"
+  "CMakeFiles/TODOApp.dir/main.cpp.o"
+  "CMakeFiles/TODOApp.dir/main.cpp.o.d"
+  "CMakeFiles/TODOApp.dir/mainwindow.cpp.o"
+  "CMakeFiles/TODOApp.dir/mainwindow.cpp.o.d"
+  "TODOApp"
+  "TODOApp.pdb"
+  "ui_mainwindow.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TODOApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
