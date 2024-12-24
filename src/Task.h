82455b848c0184db00b52761994ec621205e1c7c
Andy <andres.meneguzzo@gmail.com>
2024-12-24 11:04:25 +0100

    Adding due date and time, priority and completedStatus to the Task class.

--- src/Task.h
+++ src/Task.h
@@ -2,11 +2,13 @@
 #define TASK_H
 
 #include <string>
+#include <ctime>
 
 class Task {
 public:
     Task();
-    Task(int id, const std::string& title, const std::string& description);
+    Task(int id, const std::string& title, const std::string& description,
+         const std::time_t dueDate, int priority, bool completed);
 
     int getId() const;
     void setId(int id);
@@ -17,10 +19,23 @@ public:
     std::string getDescription() const;
     void setDescription(const std::string& description);
 
+    std::time_t getDueDate() const;
+    void setDueDate(const std::time_t dueDate);
+
+    int getPriority() const;
+    void setPriority(int priority);
+
+    bool isCompleted() const;
+    void setCompleted(bool completed);
+
+
 private:
     int m_id;
     std::string m_title;
     std::string m_description;
+    std::time_t m_dueDate;
+    int m_priority;
+    bool m_completed;
 };
 
 #endif // TASK_H
