c181387bbe8218bad8039db9998893762275bff4
Andy <andres.meneguzzo@gmail.com>
2024-12-24 11:06:15 +0100

    Task.cpp updated

--- src/Task.cpp
+++ src/Task.cpp
@@ -1,12 +1,15 @@
 #include "Task.h"
 
 Task::Task()
-    : m_id(-1), m_title(""), m_description("")
+    : m_id(-1), m_title(""), m_description(""),
+      m_dueDate(0), m_priority(1), m_completed(false)
 {
 }
 
-Task::Task(int id, const std::string& title, const std::string& description)
-    : m_id(id), m_title(title), m_description(description)
+Task::Task(int id, const std::string& title, const std::string& description,
+           const std::time_t dueDate, int priority, bool completed)
+    : m_id(id), m_title(title), m_description(description),
+      m_dueDate(dueDate), m_priority(priority), m_completed(completed) 
 {
 }
 
@@ -32,4 +35,28 @@ std::string Task::getDescription() const {
 
 void Task::setDescription(const std::string& description) {
     m_description = description;
+}
+
+std::time_t Task::getDueDate() const {
+    return m_dueDate;
+}
+
+void Task::setDueDate(const std::time_t dueDate) {
+    m_dueDate = dueDate;
+}
+
+int Task::getPriority() const {
+    return m_priority;
+}
+
+void Task::setPriority(int priority) {
+    m_priority = priority;
+}
+
+bool Task::isCompleted() const {
+    return m_completed;
+}
+
+void Task::setCompleted(bool completed) {
+    m_completed = completed;
 }
